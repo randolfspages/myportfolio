a98b1f03ac020a99ef2b9782ee37b67fe5abd924
randolf <136251983+randolfspages@users.noreply.github.com>
2024-11-04 00:15:38 -0800

    working on Nav and Side bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,59 @@
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { BsMenuUp } from "react-icons/bs";
+
+
+
 
 const Navbar = () => {
   return (
-    <nav className='fixed left-0 right-0 top-0 p-5 bg-black h-20 flex justify-between items-center'>
-        <div className='text-white'>Name</div>
-            <ul className='
-                sm:flex 
-                sm:text-white 
-                sm:justify-center 
+    <header className=' py-4 shadow-lg fixed top-0 left-0 right-0 z-100'>
+        <div className=' relative max-w-screen-lg mx-auto flex flex-col sm:flex-row py-4 sm:items-center sm:justify-between'>
+            <Link className='flex items-center px-10'>
+                <span className='mr-2'>Logo</span>
+                <span>Name</span>
+            </Link>
+
+            <input className='peer hidden' type="checkbox" id='navbar-open' />
+            <label className='sm:hidden cursor-pointer absolute mt-1 right-0 px-10' htmlFor="navbar-open">
+                <span className='sr-only'>Toggle Navigation</span>
+                <BsMenuUp />
+            </label>
+            
+            <nav aria-label='Header Navigation' className='peer-checked:block sm:block hidden py-4 sm:py-0 sm:justify-start sm:items-center'>
+                <ul className='flex flex-col  
+                sm:flex-row 
                 sm:gap-x-8 
-                sm:border 
+                gap-y-4 
+                sm:px-16  
+                items-center
+                sm:border-2 
+                sm:shadow-2xl
+                sm:drop-shadow-md 
                 sm:rounded-full 
-                sm:px-10 
                 sm:py-2
-                sm: shadow-inner bg-stone-900 p-4 rounded
-                border-stone-900
-                sm:text-sm'>
-                <li>
-                    <NavLink   NavLink to='/'>
-                        Home
-                    </NavLink>
-                </li>
+                sm: transition-all
+                sm:duration-150
+                cursor-pointer
+                '>
+                    <li className='sm:hover:scale-125 sm:hover:shadow-sm sm:duration-200'>
+                        <NavLink to="/" className="sm:text-xs">HOME</NavLink>
+                    </li>
+                    <li className='sm:hover:scale-125 sm:hover:shadow-sm sm:duration-200'>
+                        <NavLink to="/profile" className="sm:text-xs">PROFILE</NavLink>
+                    </li>
+                    <li className='sm:hover:scale-125 sm:hover:shadow-sm sm:duration-200'>
+                        <NavLink to="/portfolio" className="sm:text-xs">PORTFOLIO</NavLink>
+                    </li>
+                    <li className='sm:hover:scale-125 sm:hover:shadow-sm sm:duration-200'>
+                        <NavLink to="/contact" className="sm:text-xs">CONTACT</NavLink>
+                    </li>
+                </ul>
+            </nav>
 
-                <li>
-                    <NavLink to='/profile'>
-                        Profile
-                    </NavLink>
-                </li>
+        </div>
+    </header>
 
-                <li>
-                    <NavLink to='/portfolio'>
-                        Portfolio
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to='/contact'>
-                        Contact
-                    </NavLink>
-                </li>
-            </ul>
-            <div className='text-white'>menu-icon</div>
-    </nav>
+    
   )
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@
 
 const SideBar = () => {
   return (
-    <div className='mt-20'>
+    <div className='mt-40 fixed px-8 max-h-50 border-2'>
       SideBar
     </div>
   )
